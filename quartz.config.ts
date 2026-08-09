3d1d44cbbbab85c7bba12feed6e142f8dde3386d
Emiliano Gandini Outeda <emiliano.gandini@protonmail.com>
2026-08-09 00:34:39 -0300

    Update baseUrl to equinox.emiliano-go.com

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "equinox.emiliano-gandini-outeda.uy",
+    baseUrl: "equinox.emiliano-go.com",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
